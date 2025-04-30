8b6e77dd5bc9da5495d2785aad56ec6aac015ed6
ryan <ryannesh2376@gmail.com>
2025-04-30 23:22:54 +0300

    js1

--- code.js
+++ code.js
@@ -1,8 +1,32 @@
-console.log("hello world");
+// let message = {
+//     name: 'Ryan',
+//     country: 'Kenya',
+//     ID: 39302928
+// }
 
-let message;
+// console.log(message.ID);
 
-message = "hey Ryan"
+// let myArray = ['ryan','peshie']
 
-console.log(typeof(message));
+// console.log(myArray.length);
+// console.log(myArray[0]);
 
+// function test(name){
+//     console.log('Hello ' + name);
+// }
+
+// test('Ryan')
+
+// function divide(num1, num2){
+//     let product = num1 / num2
+//     return product
+// }
+
+// console.log(divide(5, 4));
+
+// let num = 2;
+let marks = 56;
+
+const grade = marks > 80 ? 'A' : 'pass'
+
+console.log(grade);
